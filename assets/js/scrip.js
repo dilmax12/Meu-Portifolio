ebf216762c6e25304f2fdcda445f9964c8c993c6
Adilson Nunes <DIL_MAX@hotmail.com>
2022-10-28 11:25:20 -0300

    Add Readme.md

---
+++ assets/js/scrip.js
@@ -0,0 +1,22 @@
+myinterval = setInterval(setImage, 2000);
+
+function setImage() {
+    let x = document.querySelector(".right__side");
+    x.style.backgroundImage = x.style.backgroundImage == 'url("../assets/images/Hero2.png")' ? 'url("../assets/images/Hero.png")' : 'url("../assets/images/Hero2.png")';
+}
+document.querySelector(".right__side").style.backgroundImage = 'url("../assets/images/Hero.png")'
+
+var menuOpn = document.querySelector(".menu");
+var menuButton = document.querySelector(".menu__hiden");
+
+function menuOpener() {
+
+    if (menuButton.style.display === "flex") {
+        menuButton.style.display = "none";
+    }else {
+        menuButton.style.display = "flex";
+    }
+}
+
+
+menuOpn.addEventListener("click", menuOpener);
