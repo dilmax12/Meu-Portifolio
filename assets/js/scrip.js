fcd8fa0237fa447bee4ff1335478c2a68299fb5b
Adilson Nunes <DIL_MAX@hotmail.com>
2022-10-28 11:49:42 -0300

    Corrigindo Imagens

--- assets/js/scrip.js
+++ assets/js/scrip.js
@@ -2,9 +2,9 @@ myinterval = setInterval(setImage, 2000);
 
 function setImage() {
     let x = document.querySelector(".right__side");
-    x.style.backgroundImage = x.style.backgroundImage == 'url("../assets/images/Hero2.png")' ? 'url("../assets/images/Hero.png")' : 'url("../assets/images/Hero2.png")';
+    x.style.backgroundImage = x.style.backgroundImage == 'url("./assets/images/Hero2.png")' ? 'url("./assets/images/Hero.png")' : 'url("./assets/images/Hero2.png")';
 }
-document.querySelector(".right__side").style.backgroundImage = 'url("../assets/images/Hero.png")'
+document.querySelector(".right__side").style.backgroundImage = 'url("./assets/images/Hero.png")'
 
 var menuOpn = document.querySelector(".menu");
 var menuButton = document.querySelector(".menu__hiden");
